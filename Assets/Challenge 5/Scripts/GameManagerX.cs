57f5eb1566b10bfe84591f8cc5c94b14a3129328
Fabián E. García <fabianegl79@gmail.com>
2024-02-29 20:17:37 -0300

    tiempo de comida en pantalla y puntaje arreglado

--- Assets/Challenge 5/Scripts/GameManagerX.cs
+++ Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -23,12 +23,14 @@ public class GameManagerX : MonoBehaviour
     private float minValueY = -3.75f; //  y value of the center of the bottom-most square
     
     // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
-    public void StartGame()
+    public void StartGame(int dificulty)
     {
-        spawnRate /= 5;
+        spawnRate /= dificulty;
         isGameActive = true;
-        StartCoroutine(SpawnTarget());
         score = 0;
+
+        StartCoroutine(SpawnTarget());
+        
         UpdateScore(0);
         titleScreen.SetActive(false);
     }
@@ -70,14 +72,14 @@ public class GameManagerX : MonoBehaviour
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "score";
+        scoreText.text = "score: " + score;
     }
 
     // Stop game, bring up game over text and restart button
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
-        restartButton.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(true);
         isGameActive = false;
     }
 
